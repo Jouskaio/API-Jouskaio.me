ef39864fee9d0fdeb1ea6e1656bdeb674a1ad6c8
Jouskaio <jouskaio.me@gmail.com>
2024-02-28 17:40:53 +0100

    Delete CORS

--- index.mjs
+++ index.mjs
@@ -5,24 +5,7 @@ import cors from 'cors';
 // Configure dotenv to read .env file
 dotenv.config();
 const app = express()
-// Define a list of allowed origins
-const allowedOrigins = ["http://localhost:3000", "https://jouskaio.me", process.env.API_URL ];
 
-// Configure CORS to allow requests from specific origins
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: "GET,POST", // Allow only GET and POST methods
-  allowedHeaders: "Content-Type,Authorization", // Allow only specific headers
-};
-
-// Enable CORS with the specified options
-app.use(cors(corsOptions));
 // Middleware to analyse JSON data in request body
 app.use(express.json());
 
